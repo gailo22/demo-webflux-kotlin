23ba878a14aeb4349a7a9f04b2c2580b32ff6434
montree <montree@th.ibm.com>
2018-11-22 15:28:13 +0700

    add swagger

---
+++ src/main/kotlin/com/example/demowebfluxkotlin/Env.kt
@@ -0,0 +1,10 @@
+package com.example.demowebfluxkotlin
+
+import io.github.cdimascio.dotenv.dotenv
+import io.github.cdimascio.swagger.Validate
+
+val validate = Validate.configure("static/api.yaml") { status, messages ->
+    Error(status.value(), messages)
+}
+
+val dotenv = dotenv()
