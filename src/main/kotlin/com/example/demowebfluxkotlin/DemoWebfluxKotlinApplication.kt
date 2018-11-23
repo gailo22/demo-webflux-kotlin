772bacf5057b253323cdca18677e6cfaa86ad72d
montree <montree@th.ibm.com>
2018-11-23 11:57:45 +0700

    update api

--- src/main/kotlin/com/example/demowebfluxkotlin/DemoWebfluxKotlinApplication.kt
+++ src/main/kotlin/com/example/demowebfluxkotlin/DemoWebfluxKotlinApplication.kt
@@ -1,5 +1,9 @@
 package com.example.demowebfluxkotlin
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.cdimascio.dotenv.dotenv
+import io.github.cdimascio.swagger.Validate
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -9,10 +13,13 @@ import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Mono
 import java.time.Duration
 import java.time.LocalDate
+
 
 @SpringBootApplication
 class DemoWebfluxKotlinApplication
@@ -43,16 +50,33 @@ class PersonRoutes(private val handler: PersonHandler) {
         "/person".nest {
             GET("/{id}", handler::readOne)
             GET("/", handler::readAll)
+            POST("/", handler::create)
         }
     }
 }
 
 class PersonHandler(private val personRepository: PersonRepository) {
-    fun readAll(request: ServerRequest) = ServerResponse.ok().body(personRepository.findAll())
-    fun readOne(request: ServerRequest) = ServerResponse.ok().body(personRepository.findById(request.pathVariable("id").toLong()))
+    fun readAll(request: ServerRequest) =
+        ServerResponse.ok().body(personRepository.findAll())
+    fun readOne(request: ServerRequest) =
+        ServerResponse.ok().body(personRepository.findById(request.pathVariable("id").toLong()))
+    fun create(request: ServerRequest): Mono<ServerResponse> =
+        validate.request(request).withBody(Person::class.java) {
+            ok().body(personRepository.save(it))
+        }
 }
 
 @Document
-class Person(@Id val id: Long, val firstName: String, val lastName: String, val birthdate: LocalDate? = null)
+class Person(@Id val id: Long,
+             val firstName: String,
+             val lastName: String,
+//             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+             val birthdate: LocalDate? = null)
 
 interface PersonRepository : ReactiveMongoRepository<Person, Long>
+
+val validate = Validate.configure("static/api.yaml") { status, messages ->
+    Error(status.value(), messages)
+}
+
+val dotenv = dotenv()
--- src/main/kotlin/com/example/demowebfluxkotlin/Env.kt
+++ src/main/kotlin/com/example/demowebfluxkotlin/Env.kt
@@ -3,8 +3,8 @@ package com.example.demowebfluxkotlin
 import io.github.cdimascio.dotenv.dotenv
 import io.github.cdimascio.swagger.Validate
 
-val validate = Validate.configure("static/api.yaml") { status, messages ->
-    Error(status.value(), messages)
-}
-
-val dotenv = dotenv()
+//val validate = Validate.configure("static/api.yaml") { status, messages ->
+//    Error(status.value(), messages)
+//}
+//
+//val dotenv = dotenv()
